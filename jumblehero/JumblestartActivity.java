0475ea69971a7c77dc3a8f265a4c2ab14042f388
Ratnakirti <44190671+Ratnakirti@users.noreply.github.com>
2018-10-16 15:17:55 +0530

    Update jumblehero/JumblestartActivity.java

---
+++ jumblehero/JumblestartActivity.java
@@ -0,0 +1,34 @@
+package com.example.jumblehero;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class JumblestartActivity extends Activity implements OnClickListener {
+	Button b;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_jumblestart);
+		b=(Button)findViewById(R.id.button1);
+		b.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(b==v)
+		{
+			Toast.makeText(getApplicationContext(),"STARTING GAME...", Toast.LENGTH_SHORT).show();
+			Intent i=new Intent(JumblestartActivity.this,Jumble01Activity.class);
+			startActivity(i);
+		}
+	}
+}
